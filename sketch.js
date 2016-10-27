d35f2d4a2708ded42a8a7fed908900932d5d6495
Brendan Hoppe <277467@bwls.net>
2016-10-27 11:58:12 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,12 @@
-//INTERNET SUCKS
-function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+void setup() {
+  size(640, 360, P3D);
 }
 
-function draw(){
-  background(117, 58, 61);
-  pointLight(255, 0, 0, width/2, height/2, 400);
-  rotateX(0.09 * mouseX );
-  rotateY(0.09 * mouseY );
-  fill(130,45,7);
-  sphere(130,130,100);
+void draw() {
+  background(0);
+  camera(mouseX, height/2, (height/2) / tan(PI/6), width/2, height/2, 0, 0, 1, 0);
+  translate(width/2, height/2, -100);
+  stroke(255);
+  noFill();
+  box(200);
 }
