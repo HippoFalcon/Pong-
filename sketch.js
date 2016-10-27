61a20d68e7401d1ba1802787ae57c82c241bc210
Brendan Hoppe <277467@bwls.net>
2016-10-27 11:00:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 //INTERNET SUCKS
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+  pointLight(255, 0, 0, width/2, height/2, 400);
 }
 
 function draw(){
   background(117, 58, 61);
   rotateX(0.09 * mouseX );
   rotateY(0.09 * mouseY );
+  fill(130,45,7)
   sphere(130,130,100);
 }
